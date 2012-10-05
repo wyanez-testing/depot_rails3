9eb2cc436fe58a0ef3543975f76fe5ab868b56ad
William Yanez <wyanez@gmail.com>
2012-10-04 20:36:01 -0430

    Commit Inicial. Creacion del Proyecto en Rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotRails3::Application.config.secret_token = '041f645e737f9f8a8c4a463c25c0cc0688676b12046dd703e70b3ce77bbd81b50cce928bef21ed8090ca24214188039931706e9307d8bedc1261eba8338f45e9'
