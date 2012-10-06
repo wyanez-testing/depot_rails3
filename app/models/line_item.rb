b4e7b331dbdff7975e6e7252389e007ef3f808bf
William Yanez Testing <wyanez.chess@gmail.com>
2012-10-06 15:34:25 -0430

    Iteracion E3: SE mejoró la funcionalidad del carrito. Se agregó un boton para vaciar el carrito, se muestra la columna precio y el total de la compra. Se mejoró la presentación del carrito

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
   belongs_to :cart
   belongs_to :product  
+
+  def total_price
+    product.price * quantity
+  end 
+
 end
