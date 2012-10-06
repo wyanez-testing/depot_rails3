18bb6ed48c0a272bd49d427c426fc9eb68051550
William Yanez Testing <wyanez.chess@gmail.com>
2012-10-06 19:05:48 -0430

    Iteracion F2: Se implementaron las funcionalidades de Agregar/Remover del carrito con Ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,6 +47,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
@@ -75,12 +76,13 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     #@line_item = LineItem.find(params[:id])
-    cart = current_cart
-    @line_item = cart.line_items.find(params[:id])
+    @cart = current_cart
+    @line_item = @cart.line_items.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
       format.html { redirect_to store_url, notice: 'Item removed form cart' }
+      format.js
       format.json { head :no_content }
     end
   end
