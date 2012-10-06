d8230da7f891a37402f25af79a1295a428a6d570
William Yanez Testing <wyanez.chess@gmail.com>
2012-10-06 15:50:01 -0430

    Iteracion E4: Se implementó la funcionalidad de eliminar un item del carrito

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -74,11 +74,13 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item = LineItem.find(params[:id])
+    #@line_item = LineItem.find(params[:id])
+    cart = current_cart
+    @line_item = cart.line_items.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to store_url, notice: 'Item removed form cart' }
       format.json { head :no_content }
     end
   end
